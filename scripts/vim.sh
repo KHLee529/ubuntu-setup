7ab8c93653571442b1a945a15ac71fc254994a60
KHLee529 <khlee870529@gmail.com>
2022-09-04 21:43:08 +0800

    Add `vim.sh`

---
+++ scripts/vim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$(pwd)
+
+set -e
+set -u
+set -x
+
+sudo apt -y update && sudo apt -y install vim 
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+ln ../config-files/vimrc $HOME/.vimrc
+vim -c "PlugInstall"
+
+cd $DIR

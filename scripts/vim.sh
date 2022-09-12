2d3f757f021f19b991857516104e3648642c4b92
KHLee529 <khlee870529@gmail.com>
2022-09-12 15:44:43 +0800

    Add vim-gtk for using system clipboard

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -5,8 +5,9 @@ DIR=$(pwd)
 set -e
 set -u
 set -x
-
-sudo apt -y update && sudo apt -y install vim 
+# install vim-gtk for using system clipboard
+# see https://vim.fandom.com/wiki/Accessing_the_system_clipboard
+sudo apt -y update && sudo apt -y install vim vim-gtk
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 ln -f ../config-files/vimrc $HOME/.vimrc
